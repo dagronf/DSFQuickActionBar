dc39030271485e4e0678c8cb8540282d0de3d92c
Darren Ford <dford_au-reg@yahoo.com>
2021-07-22 08:28:01 +1000

    Initial Commit

---
+++ Sources/DSFQuickActionBar/DSFQuickActionBar.swift
@@ -0,0 +1,6 @@
+public struct DSFQuickActionBar {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/DSFQuickActionBarTests/DSFQuickActionBarTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DSFQuickActionBar
+
+final class DSFQuickActionBarTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DSFQuickActionBar().text, "Hello, World!")
+    }
+}
