1adb0894d39c3a3e0afcb80a5f52e1476514eafe
Darren Ford <dford_au-reg@yahoo.com>
2024-04-06 17:15:42 +1100

    Add PrivacyInfo.xcprivacy

--- Package.swift
+++ Package.swift
@@ -23,9 +23,14 @@ let package = Package(
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "DSFQuickActionBar",
-			dependencies: ["DSFAppearanceManager"]),
+			dependencies: ["DSFAppearanceManager"],
+			resources: [
+				.copy("PrivacyInfo.xcprivacy"),
+			]
+		),
 		.testTarget(
 			name: "DSFQuickActionBarTests",
-			dependencies: ["DSFQuickActionBar"]),
+			dependencies: ["DSFQuickActionBar"]
+		),
 	]
 )
