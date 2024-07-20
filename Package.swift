7be7b3bd2cebb19ee119ba1f972ecce581de5aeb
Darren Ford <dford_au-reg@yahoo.com>
2024-07-20 17:40:40 +1000

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.4
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,18 +8,14 @@ let package = Package(
 		.macOS(.v10_13),
 	],
 	products: [
-		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(name: "DSFQuickActionBar", targets: ["DSFQuickActionBar"]),
 		.library(name: "DSFQuickActionBar-static", type: .static, targets: ["DSFQuickActionBar"]),
 		.library(name: "DSFQuickActionBar-shared", type: .dynamic, targets: ["DSFQuickActionBar"]),
 	],
 	dependencies: [
-		// Dependencies declare other packages that this package depends on.
-		.package(url: "https://github.com/dagronf/DSFAppearanceManager", from: "3.3.0"),
+		.package(url: "https://github.com/dagronf/DSFAppearanceManager", .upToNextMinor(from: "3.5.0")),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
-		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "DSFQuickActionBar",
 			dependencies: ["DSFAppearanceManager"],
